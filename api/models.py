c282c1bb60757b55fbeb7b96f07d69a83d72772b
Chuuu <dustychuuu@gmail.com>
2024-07-15 19:01:18 +0300

    models edited, serializers added

--- api/models.py
+++ api/models.py
@@ -7,15 +7,15 @@ class Posts(models.Model):
     '''Класс описывает модель постов:
     post_id - Идентификатор поста;
     title - Заголовок поста;
-    content - Содержимое поста TODO ОГРАНИЧИТЬ ДЛИНУ СУЩНОСТИ
-    picture - Изображение относящееся к посту. По умолчанию None TODO ЗАПИЛИТЬ ДОБАВЛЕНИЕ НЕСКОЛЬКИХ IMG
+    content - Содержимое поста
+    picture - Изображение относящееся к посту. Кодировка Base64. По умолчанию None TODO ЗАПИЛИТЬ ДОБАВЛЕНИЕ НЕСКОЛЬКИХ IMG
     likes - Лайки на посте TODO РЕАЛИЗОВАТЬ ОБНОВЛЕНИЕ В РЕАЛЬНОМ ВРЕМЕНИ НА УРОВНЕ БЭКЕНДА
     date_time_created - Дата и время создания поста
     date_time_edited - Дата редактирования поста TODO РЕАЛИЗОВАТЬ ТАЙМЕР СУТКИ НА РЕДАКТИРОВАНИЕ ПОСТА'''
 
     post_id = models.BigAutoField(primary_key=True, unique=True)
     title = models.CharField(max_length=256)
-    content = models.TextField(default=None)
+    content = models.TextField(max_length=1024, default=None)
     picture = models.TextField(default=None)
     likes = models.BigIntegerField(default=0)
     date_time_created = models.DateField(auto_now_add=True)
---
+++ api/serializers.py
@@ -0,0 +1,15 @@
+from rest_framework import serializers
+from .models import Posts, Comments
+
+'''Сериализаторы для моделей. Позволяют де- и сериализовать данные в JSON формат'''
+
+class PostsSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Posts
+        fields = '__all__'
+
+
+class CommentsSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Comments
+        fields = '__all__'
