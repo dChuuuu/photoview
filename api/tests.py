415df6b9257e24a5761bec56047e9a7a97492e28
Chuuu <dustychuuu@gmail.com>
2024-07-25 22:06:29 +0300

    pytest configured

--- api/tests.py
+++ api/tests.py
@@ -1,3 +1,10 @@
+from django.conf import settings
 from django.test import TestCase
-
+from .models import Comments, PostsCommentsManager
+import pytest
 # Create your tests here.
+
+def test_comment_without_post():
+
+    instance = Comments()
+    instance.objects.create(post_id=1, content='content')
