e1a88f5b6b7b589bd61b69c07791415d55c8b5ee
Chuuu <dustychuuu@gmail.com>
2024-08-13 20:49:38 +0300

    test commit

--- photoview/urls.py
+++ photoview/urls.py
@@ -19,6 +19,7 @@ from django.urls import path
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 from rest_framework import permissions
+import os
 
 
 from api.views import PostsAPIView, PostAPIView, CommentsAPIView, CommentsDeleteAPIView
