a1fb123eb65e0c77492ca118d80101ed4e61734c
Chuuu <dustychuuu@gmail.com>
2024-08-21 19:20:08 +0300

    allowed hosts edited

--- photoview/settings.py
+++ photoview/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = dotenv_values().get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['127.0.0.1', 'photoview-s88m.onrender.com']
 
 
 # Application definition
