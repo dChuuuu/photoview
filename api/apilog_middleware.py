cccd191e8bb97df68fc436e6b7450a672331d06d
Chuuu <dustychuuu@gmail.com>
2024-08-25 10:57:51 +0300

    get_response in logging edited

--- api/apilog_middleware.py
+++ api/apilog_middleware.py
@@ -4,10 +4,10 @@ logger = logging.getLogger(__name__)
 
 
 class LoggingMiddleware:
-    def __init__(self, next_middleware):
-        self.next_middleware = next_middleware
+    def __init__(self, get_response):
+        self.get_response = get_response
 
     def __call__(self, request):
         logger.info('Request: %s %s', request.method, request.path)
-        response = self.next_middleware(request)
+        response = self.get_response(request)
         return response
